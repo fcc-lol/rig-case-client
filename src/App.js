fe997cbe87375283a8b4bbbd4f26fcb7dd251b42
Leo Mancini <mancini@leo.gd>
2025-06-17 15:55:59 -0400

    Adding template

--- src/App.js
+++ src/App.js
@@ -6,13 +6,55 @@ const Page = styled.div`
   justify-content: center;
   align-items: center;
   height: 100vh;
-  font-size: 24px;
-  color: #333;
+  background-color: #000000;
+`;
+
+const Section = styled.div`
+  position: absolute;
+  border-radius: 32px;
+`;
+
+const TopSection = styled(Section)`
+  top: 8px;
+  left: 12px;
+  width: 392px;
+  height: 318px;
+  background-color: red;
+`;
+
+const MiddleSection = styled(Section)`
+  top: 406px;
+  left: 12px;
+  width: 392px;
+  height: 188px;
+  background-color: blue;
+`;
+
+const BottomLeftSection = styled(Section)`
+  top: 672px;
+  left: 12px;
+  width: 150px;
+  height: 164px;
+  background-color: green;
+`;
+
+const BottomRightSection = styled(Section)`
+  top: 672px;
+  left: 252px;
+  width: 150px;
+  height: 164px;
+  background-color: yellow;
 `;
 
 function App() {
-  return <Page>Hello world!</Page>;
+  return (
+    <Page>
+      <TopSection />
+      <MiddleSection />
+      <BottomLeftSection />
+      <BottomRightSection />
+    </Page>
+  );
 }
 
 export default App;
-
